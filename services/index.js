1ab9f7e6c140f13184e7820b2c593942a2a37514
Juliana de Assis Fernandes <julidanes@gmail.com>
2022-02-16 17:18:21 -0300

    Encontra pontos dos carros

--- services/index.js
+++ services/index.js
@@ -12,50 +12,20 @@ const getAllCheckPoints = async() => {
 };
 
 const getCarsPositionsCheckPoints = async () => {
-    const allCarsPositions = await model.getAllCarsPositions();
-    const allCheckPoints = await model.getAllCheckPoints();  
- 
-    let carsCheckPoints = allCarsPositions;
-    let checkPoints = allCheckPoints;    
-    let carPonto;
-    for (let i = 0; i <= carsCheckPoints.length; i++) {
-        let j = 0;
-        while (j < checkPoints.length) {            
-            if (
-                isInside(
-                    carsCheckPoints[i].latitude, checkPoints[j].latitude, carsCheckPoints[i].longitude, checkPoints[j].longitude, checkPoints[j].raio
-                    )) {
-                carPonto = checkPoints[j].nome;
-                carsCheckPoints[i].point = carPonto;
-                console.log(carPonto, 'dentro do for');
-                j++
-                
-            }
-            j++;
-            console.log(carPonto, 'fora do for');
-        }
-        console.log(carPonto, 'fora do while');
-
-    }
-    // for (let car of carsCheckPoints) {
-    //     let i = 0;
-    //     while (i < checkPoints.length) {            
-    //         if (
-    //             isInside(
-    //                 car.latitude, checkPoints[i].latitude, car.longitude, checkPoints[i].longitude, checkPoints[i].raio
-    //                 )) {
-    //             carPonto = checkPoints[i].nome;
-    //             car.ponto = carPonto;
-    //             console.log(carPonto, 'dentro do for');
-    //             i++;
-    //         }
-    //         i++;
-    //         console.log(carPonto, 'fora do for');
-    //     }
-    //     console.log(carPonto, 'fora do while');
-    // }
+    let cars = await getAllCarsPositions();
+    let points = await getAllCheckPoints();
+    let carsPoints = [];
     
-    return carsCheckPoints;
-}
+    cars.forEach((car) => {
+        points.forEach((point) => {
+            if (isInside (car.latitude, point.ponto_latitude, car.longitude, point.ponto_longitude, point.raio)) {
+                car.ponto = point.nome;
+                carsPoints.push(car);
+            }
+        })
+    });    
+    
+    return carsPoints;
+};
 
 module.exports = { getAllCarsPositions, getAllCheckPoints, getCarsPositionsCheckPoints };
--- utils/index.js
+++ utils/index.js
@@ -1,23 +1,26 @@
-const getArea = (latA, latB, longA, longB) => {
-    const area = Math.sqrt(
-        (Math.abs(latA) - Math.abs(latB))**2 + (Math.abs(longA) - Math.abs(longB))**2);
-    return area;
-}
+const isInside = (latA, latB, longA, longB, rad) => {
+    const latARad = Math.abs(latA * Math.PI/180);
+    const latBRad = Math.abs(latB * Math.PI/180);
+    const longARad = Math.abs(longA * Math.PI/180);
+    const longBRad = Math.abs(longB * Math.PI/180);
+    const radioKm = rad / 1000;    
+    const dLat = Math.abs(latARad - latBRad);
+    const dLon = Math.abs(longARad - longBRad);
+    const radioEarth = 6371;    
 
+    const a = Math.pow(Math.sin(dLat/2), 2) + Math.cos(latARad) * Math.cos(latBRad) * Math.pow(Math.sin(dLon/2), 2);
+    const b = 2 * Math.asin(Math.sqrt(a));    
 
+    const distance = b * radioEarth;    
 
-console.log(getArea(-402, -200, -2231, -1000));
-
-const isInside = (latA, latB, longA, longB, rad) => {     
-    
-    if((Math.abs(latA) - Math.abs(latB))**2 + (Math.abs(longA) - Math.abs(longB))**2 <= rad**2){
+    if (distance <= radioKm) {
         return true;
-    }    
-    else {
+    } else {
         return false;
-    }
+    } 
 }
 
-module.exports = { getArea, isInside };
+module.exports = { isInside };
 
-// https://www.geeksforgeeks.org/find-if-a-point-lies-inside-or-on-circle/
+//Referência para fórmula de distância:
+//https://www.geeksforgeeks.org/program-distance-two-points-earth/#:~:text=For%20this%20divide%20the%20values,is%20the%20radius%20of%20Earth
